47a2cbffcba198bab34a30d8e05ed904d4c8f047
iamprayush <iamprayush@gmail.com>
2021-05-27 21:38:27 +0530

    Added login views.

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('register/', views.Register.as_view(), name='register'),
-    path('login/', views.Login.as_view(), name='login'),
+    path('', include('django.contrib.auth.urls')),
 ]

b132c3a3b4654493c12c6c3be20fca610da808e9
iamprayush <iamprayush@gmail.com>
2021-05-27 18:51:30 +0530

    Added barebones of users app.

---
+++ users/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views import View
+
+class Register(View):
+    def get(self, request):
+        pass
+    
+    def post(self, request):
+        pass
+
+class Login(View):
+    def get(self, request):
+        pass

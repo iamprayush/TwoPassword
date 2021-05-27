7d5759135a232da35b393cc73d997ad3ae9d7c7d
iamprayush <iamprayush@gmail.com>
2021-05-27 21:20:15 +0530

    Added register form with views.

--- users/views.py
+++ users/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import RegisterForm
+
 
 class Register(View):
     def get(self, request):
-        return render(request, 'users/register.html')
-    
+        form = RegisterForm()
+        return render(request, 'users/register.html', {"form": form})
+
     def post(self, request):
-        pass
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return render(request, 'users/register.html', {"form": form})
+
 
 class Login(View):
     def get(self, request):

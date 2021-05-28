dcb317373ab9a85a52df7ffc6913b049a3ef6e72
iamprayush <iamprayush@gmail.com>
2021-05-28 18:12:37 +0530

    Added 'two-password.herokuapp.com' to allowed hosts.

--- twopassword/settings.py
+++ twopassword/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9^8yo&k)4zy&@oj7o&#9$p%_@0okzs66cd4e9^yw9p0p3ew=d%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['two-password.herokuapp.com']
 
 
 # Application definition

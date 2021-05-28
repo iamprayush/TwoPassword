1bfd631b048559be30f6569aecdd6e36345346b0
iamprayush <iamprayush@gmail.com>
2021-05-28 18:26:07 +0530

    Temporarily set DEBUG=True.

--- twopassword/settings.py
+++ twopassword/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9^8yo&k)4zy&@oj7o&#9$p%_@0okzs66cd4e9^yw9p0p3ew=d%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['two-password.herokuapp.com']
+ALLOWED_HOSTS = ['two-password.herokuapp.com', 'localhost']
 
 
 # Application definition
